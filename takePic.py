efe5bb0a3a621bb04bdc58bd40d0a4f643cc31d6
Nicolas Felipe Vergara Duran <nf.vergara10@uniandes.edu.co>
2019-03-23 17:51:54 -0500

    popis3

--- takePic.py
+++ takePic.py
@@ -1,18 +1,26 @@
 import align_faces as af
 import cv2
 import time
+import subprocess
 
 cap = cv2.VideoCapture(0)
 j=1
-time.sleep(10)
+time.sleep(3)
 while(True):
 	# Capture frame-by-frame
     ret, frame = cap.read()
     # cv2.imwrite('pic2_{0}.png'.format(j),frame)
     al_frame=af.faceAlign(frame)
-    cv2.imwrite('pic_al_{0}.png'.format(j),al_frame)
+    cv2.imwrite('foto.png'.format(j),al_frame)
     j=j+1
     # Display the resulting frame
     cv2.imshow('frame', frame)
     cv2.imshow('alignedframe', al_frame)
-    cv2.waitKey(0)
+    
+    break
+
+# When everything done, release the capture
+cap.release()
+cv2.imshow('frame', frame)
+cv2.destroyAllWindows()
+subprocess.call(['./upload.sh'])
---
+++ upload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add foto.png
+git commit -m 'popis2'
+git push
