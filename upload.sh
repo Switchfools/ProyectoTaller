7e765a110a5cf39c4ae5fb70a88746b67cb8ba9f
Nicolas Felipe Vergara Duran <nf.vergara10@uniandes.edu.co>
2019-04-02 15:43:13 -0500

    autoupdate 2019-04-02-15:43:13

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+mv FotosEntrada/foto5.png FotosEntrada/`date +%F-%T`.png
 git add *
-#mv FotosEntrada/foto5.png FotosEntrada/`date +%F-%T`.png
 git commit -m "autoupdate `date +%F-%T`"
 git push
