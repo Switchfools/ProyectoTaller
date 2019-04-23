778600ecf72081a6078757774d2780fce472721d
Switchfools <nf.vergara10@uniandes.edu.co>
2019-04-23 17:17:24 -0500

    autoupdate 2019-04-23-17:17:24

--- takePic.py
+++ takePic.py
@@ -6,18 +6,19 @@ import faceRecognition as fr
 import pymysql
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-
+'''
 connection= pymysql.connect(host='192.168.0.100',
                             user='raspberry',
                             password='0000',
                             db='casa')
+'''
 cap = PiCamera()
 cap.resolution = (640, 480)
 cap.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(cap, size=(640, 480))
 j=1
 time.sleep(3)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in cap.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# Capture frame-by-frame
     frame = frame.array
     faces,gray = fr.faceDetection(frame)
@@ -33,10 +34,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     rawCapture.truncate(0)
 
 # When everything done, release the capture
-cap.release()
 #cv2.imshow('frame', frame)
 cv2.destroyAllWindows()
 subprocess.call(['./upload.sh'])
+'''
 with connection:
     cur = connection.cursor()
     cur.execute("SELECT * FROM imagenes ORDER BY id DESC LIMIT 1;")
@@ -46,3 +47,4 @@ with connection:
         print("{0}".format(row[0]))
     id_nuevo=int(id_actual[0])+1
     cur.execute("INSERT INTO imagenes VALUES("+str(id_nuevo)+");")
+'''
