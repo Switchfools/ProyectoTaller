b527131b6e44a5b06d48ff6884c1f761d0056e38
Nicolas Felipe Vergara Duran <nf.vergara10@uniandes.edu.co>
2019-04-23 17:11:49 -0500

    new2

--- takePic.py
+++ takePic.py
@@ -2,6 +2,7 @@ import align_faces as af
 import cv2
 import time
 import subprocess
+import faceRecognition as fr
 import pymysql
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -19,15 +20,17 @@ time.sleep(3)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# Capture frame-by-frame
     frame = frame.array
+    faces,gray = fr.faceDetection(frame)
     # cv2.imwrite('pic2_{0}.png'.format(j),frame)
     #al_frame=af.faceAlign(frame)
-    cv2.imwrite('FotosEntrada/foto5.png'.format(j),frame)
+    if len(faces) > 0:
+        cv2.imwrite('FotosEntrada/foto5.png'.format(j),frame)
+        break
     j=j+1
     # Display the resulting frame
     #cv2.imshow('frame', frame)
     #cv2.imshow('alignedframe', al_frame)
     rawCapture.truncate(0)
-    break
 
 # When everything done, release the capture
 cap.release()
