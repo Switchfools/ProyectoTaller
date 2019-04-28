bdea072fd62a7038aee30401f0f7f1e166fde9ef
Nicolas Felipe Vergara Duran <nf.vergara10@uniandes.edu.co>
2019-04-28 17:24:41 -0500

    changes5

--- tomarFoto.py
+++ tomarFoto.py
@@ -19,8 +19,11 @@ for frame in cap.capture_continuous(rawCapture, format="bgr", use_video_port=Tru
 	cv2.imwrite('pic_al_{0}.png'.format(j),frame)
 	j=j+1
 	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	cv2.waitKey(0)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
     rawCapture.truncate(0)
+    if (j=23)
+        break
 
+GPIO.output(23, GPIO.OUT)
 cv2.destroyAllWindows()
