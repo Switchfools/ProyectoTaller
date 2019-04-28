911d5e3f9a2b912a77735799f4cb2fc02eb9c22a
Nicolas Felipe Vergara Duran <nf.vergara10@uniandes.edu.co>
2019-04-28 16:42:06 -0500

    changes2

--- tomarFoto.py
+++ tomarFoto.py
@@ -11,7 +11,7 @@ cap.resolution = (640, 480)
 cap.framerate = 32
 rawCapture = PiRGBArray(cap, size=(640, 480))
 j=1
-while(True):
+for frame in cap.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# Capture frame-by-frame
 	frame = frame.array
 	cv2.imwrite('pic_al_{0}.png'.format(j),frame)
