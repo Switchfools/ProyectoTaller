295aff45f808c627369299daeb09877d6a65e7bb
Switchfools <nf.vergara10@uniandes.edu.co>
2019-04-28 17:32:51 -0500

    ch

--- tomarFoto.py
+++ tomarFoto.py
@@ -14,16 +14,16 @@ cap.framerate = 30
 rawCapture = PiRGBArray(cap, size=(1080 , 720))
 j=1
 for frame in cap.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# Capture frame-by-frame
-	frame = frame.array
-	cv2.imwrite('pic_al_{0}.png'.format(j),frame)
-	j=j+1
-	# Display the resulting frame
+    # Capture frame-by-frame
+    frame = frame.array
+    cv2.imwrite('pic_al_{0}.png'.format(j),frame)
+    j=j+1
+    # Display the resulting frame
     cv2.imshow('frame', frame)
     cv2.waitKey(0)
     rawCapture.truncate(0)
     if (j=23)
         break
 
-GPIO.output(23, GPIO.OUT)
+GPIO.output(23, GPIO.LOW)
 cv2.destroyAllWindows()
