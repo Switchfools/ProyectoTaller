bbba02c275e1d64e2f46a7b03146a6f530d89fd1
switchfools <nf.vergara10@uniandes.edu.co>
2019-04-30 14:40:13 -0500

    autoupdate 2019-04-30-14:40:12

--- abrirPuerta.py
+++ abrirPuerta.py
@@ -1,10 +1,15 @@
 import pymysql
 import os
+import RPi.GPIO as GPIO
+import time
 connection= pymysql.connect(host='192.168.0.100',
                             user='rasp',
                             password='0000',
                             db='casa')
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 with connection:
         cur = connection.cursor()
         cur.execute("SELECT * FROM entradas ORDER BY fecha DESC LIMIT 1;")
@@ -26,9 +31,15 @@ while(True):
     if(fecha_actual!=fecha_nueva):
         if(persona!='Desconocido'):
             print('Puerta abierta')
+            GPIO.output(24, GPIO.HIGH)
+            time.sleep(5)
+            GPIO.output(24, GPIO.LOW)
     
         else:
             print('Acceso no autorizado')
+            GPIO.output(22, GPIO.HIGH)
+            time.sleep(5)
+            GPIO.output(22, GPIO.LOW)
         fecha_actual=fecha_nueva
         os.system('./rm.sh') 
         break
